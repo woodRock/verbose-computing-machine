0a699f60260b27d9d331cd10f1d5728edb9145e2
Jesse Wood <woodRock@users.noreply.github.com>
2020-12-07 03:43:06 +1300

    Update part-1.hs

--- day-4/part-1.hs
+++ day-4/part-1.hs
@@ -1,15 +1,6 @@
 import Data.List 
 import Data.List.Split
 
--- byr (Birth Year)
--- iyr (Issue Year)
--- eyr (Expiration Year)
--- hgt (Height)
--- hcl (Hair Color)
--- ecl (Eye Color)
--- pid (Passport ID)
--- cid (Country ID)
-
 check :: String -> Bool
 check s = all (== True) $ map (\x -> x `isInfixOf` s) required
     where required = ["byr","iyr","eyr","hgt","hcl","ecl","pid"]
