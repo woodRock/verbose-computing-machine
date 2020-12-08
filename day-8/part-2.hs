adf1b439e0054d3eadcb8c13e479706df27e6e07
woodRock <j.r.h.wood98@gmail.com>
2020-12-08 21:29:17 +1300

    Refactoring

--- day-8/part-2.hs
+++ day-8/part-2.hs
@@ -1,5 +1,6 @@
 import Text.Read 
 import Text.Regex.Posix
+import Data.Maybe
 
 type Line = String
 type Instruction = (String,Char,Int)
@@ -7,22 +8,21 @@ type Accumulator = Int
 type Index = Int
 type Visited = [Int]
 
-run :: Instruction -> Index -> Accumulator -> (Index, Accumulator)
-run ("nop",_,_) i a = (i + 1, a)
-run ("acc",'+',x) i a = (i + 1, a + x)
-run ("acc",'-',x) i a = (i + 1, a - x)
-run ("jmp",'+',x) i a = (i + x, a)
-run ("jmp",'-',x) i a = (i - x, a)
+update :: Line -> Line 
+update l 
+    | op == "jmp" = "nop" ++ r
+    | otherwise = "jmp" ++ r
+    where 
+        (op,r) =  splitAt 3 l 
 
-execute :: Index -> [Instruction] -> Accumulator -> Visited -> (Bool, Accumulator) 
-execute i b a v
-    | alreadyVisited = (False , a)
-    | terminates = (True , a)
-    | otherwise = execute n b da (i:v)
-        where
-            (n,da) = run (b !! i) i a
-            alreadyVisited = i `elem` v
-            terminates = i == length b - 1
+possible :: [Line] -> [[Line]]
+possible b = map (\u -> map (uncurry (updateIfIndex u)) lines) idx
+    where 
+        idx = map snd $ filter ((=~ "(nop|jmp)") . fst) lines
+        lines = zip b [0..length b - 1] 
+        updateIfIndex u l i 
+            | i == u = update l 
+            | otherwise = l
 
 parse :: Line -> Instruction
 parse bc = (op, sign, arg)
@@ -32,25 +32,29 @@ parse bc = (op, sign, arg)
         sign = head $ s !! 1
         s = words bc
 
-possible :: [Line] -> [[Line]]
-possible b = map (\u -> map (\(l,i) -> if i == u then update l else l) lines) idx
-    where 
-        idx = map snd $ filter ((=~ "(nop|jmp)") . fst) lines
-        lines = zip b [0..length b - 1] 
+run :: Instruction -> Index -> Accumulator -> (Index, Accumulator)
+run ("nop",_,_) i a = (i + 1, a)
+run ("acc",'+',x) i a = (i + 1, a + x)
+run ("acc",'-',x) i a = (i + 1, a - x)
+run ("jmp",'+',x) i a = (i + x, a)
+run ("jmp",'-',x) i a = (i - x, a)
 
-update :: Line -> Line 
-update l 
-    | op == "jmp" = "nop" ++ r
-    | otherwise = "jmp" ++ r
-    where 
-        (op,r) =  splitAt 3 l 
+execute :: Index -> [Instruction] -> Accumulator -> Visited -> Maybe Accumulator 
+execute i b a v
+    | infiniteLoop = Nothing
+    | terminates = Just a
+    | otherwise = execute n b da (i:v)
+        where
+            (n,da) = run (b !! i) i a
+            infiniteLoop = i `elem` v
+            terminates = i == length b - 1
 
 solve :: [Line] -> Maybe Accumulator 
 solve bc 
     | null tmp = Nothing 
-    | otherwise = Just $ snd $ head tmp
+    | otherwise = head tmp
     where 
-        tmp = filter fst x
+        tmp = filter isJust x
         x = map (\l -> execute 0 l 0 []) trials 
         trials = map (map parse) $ possible bc
 
