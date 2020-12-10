52429c2e99e1aae46e3deb7ceab7ee96f103dafb
woodRock <j.r.h.wood98@gmail.com>
2020-12-10 22:33:43 +1300

    Refactor part 1

--- day-10/part-1.hs
+++ day-10/part-1.hs
@@ -1,13 +1,8 @@
 import Data.List
 
-check :: [Int] -> (Int, Int)
-check a = (ones, threes)  
-    where
-        [ones, threes] = map length $ group $ sort $ zipWith (-) (tail l) l 
-        l = 0 : a ++ [maximum a + 3]
-
-solve :: [Int] -> Int 
-solve  = uncurry (*) . check . sort
+solve :: [Int] -> Int
+solve a = product $ map length $ group $ sort $ zipWith (-) (tail l) l 
+    where l = 0 : a ++ [maximum a + 3]
 
 main :: IO () 
-main = interact $ show . solve . map read . lines 
+main = interact $ show . solve . sort . map read . lines 
