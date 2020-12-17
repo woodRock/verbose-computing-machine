598e3a3b0eba5691c0f38b59f39e2797cb6cca82
woodRock <j.r.h.wood98@gmail.com>
2020-12-17 23:56:52 +1300

    Day 16

--- day-16/part-2.hs
+++ day-16/part-2.hs
@@ -10,10 +10,13 @@ type Rule = [Range]
 type Range = (Int,Int)
 
 -- TODO Discard invalid values
+parseDepart :: String -> [Rule]
+parseDepart = map ((\[a,b,c,d] -> [(read a,read b),(read c,read d)]) . 
+            concatMap (splitOn "-") . filter (/= "or") . words . last . splitOn ":") . filter (=~ "departure") . lines
 
 parseRules :: String -> [Rule]
 parseRules = map ((\[a,b,c,d] -> [(read a,read b),(read c,read d)]) . 
-            concatMap (splitOn "-") . filter (/= "or") . words . last . splitOn ":") . filter (=~ "departure") . lines
+            concatMap (splitOn "-") . filter (/= "or") . words . last . splitOn ":") . lines
 
 parseTicket :: String -> [Ticket] 
 parseTicket = map (map read . splitOn ",") . tail . lines
@@ -23,15 +26,20 @@ checkCol col rules = any (all (== True)) (transpose j)
     where
         j = map (\i -> map (\[(a,b),(c,d)] -> (a <= i && i <= b) || (c <= i && i <= d) ) rules) col
 
+discard :: [Ticket] -> [Rule] -> [Ticket]
+discard ts r = filter go ts
+    where
+        go = not . any (== True) . map (\x -> all (== False) $ map (\(a,b) -> a <= x && x <= b ) (concat r))
+
 check :: [Column] -> [Rule] -> [Int]
 check cols rules = map fromJust $ filter (/= Nothing) $ map (\(c,i) -> if checkCol c rules then Just i else Nothing) (zip cols [0..]) 
 
-solve :: [String] -> [[Int]] 
-solve s = n -- product $ map (\i -> m !! i) $ check n r 
+solve :: [String] -> [Int] 
+solve s = check n r-- product $ map (\i -> m !! i) $ check n r 
     where
-        r = parseRules rules 
+        r = parseDepart rules 
         m = head $ parseTicket mine
-        n = transpose $ parseTicket nearby
+        n = transpose $ discard (parseTicket nearby) (parseRules rules)
         [rules,mine,nearby] = s
 
 main :: IO ()
