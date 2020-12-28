90fc8efef0054254f30158bb872c824e2dd8d3be
woodRock <j.r.h.wood98@gmail.com>
2020-12-29 01:19:43 +1300

    Refactor

--- day-23/part-1.hs
+++ day-23/part-1.hs
@@ -3,9 +3,6 @@ import Data.List (intersperse,maximum,(\\))
 parse :: String -> [Int]
 parse = map read . words . intersperse ' '
 
-makeHead :: Int -> [Int] -> [Int]
-makeHead i s = take (length s) . dropWhile (/= i) . cycle $ s
-
 three :: [Int] -> [Int]
 three = take 3 . drop 1
 
@@ -17,14 +14,15 @@ dec n a
     where
         next = n - 1
 
+makeHead :: Int -> [Int] -> [Int]
+makeHead i s = take (length s) . dropWhile (/= i) . cycle $ s
+
 turn :: [Int] -> [Int]
-turn a@(x:xs) = makeHead n res
+turn a = makeHead n res
     where 
         n = a !! 4
-        res = di : three a ++ takeWhile (/= di) (drop 1 $ cycle rest)
-        rest = filter (\x -> x `notElem` three a) (di:r)
-        (di:r) = makeHead d a
-        d = dec x a 
+        res = head r : three a ++ takeWhile (/= head r) (drop 1 $ cycle r)
+        r = makeHead (dec (head a) a) a \\ three a
 
 solve :: [Int] -> String
 solve = foldl (\acc i -> acc ++ show i) "" . tail . makeHead 1 . (!! 100) . iterate turn 
