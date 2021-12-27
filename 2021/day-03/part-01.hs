8c6af8ca7ded1b7aba9df3ff486e0222af4fd9c6
woodrock <j.r.h.wood98@gmail.com>
2021-12-27 21:21:22 +1300

    lint

--- 2021/day-03/part-01.hs
+++ 2021/day-03/part-01.hs
@@ -2,16 +2,16 @@ import Data.List (transpose)
 import Data.Char (digitToInt)
 
 binarytoint :: [Int] -> Int 
-binarytoint a = sum $ map (\(x,y) -> y*2^x) $ zip (reverse [0..n]) a
+binarytoint a = sum $ map (\(x,y)-> y*2^x) $ zip (reverse [0..n]) a
   where 
-    n = (length a) - 1
+    n = length a - 1
 
 solve :: [[Int]] -> Int 
-solve x = product $ map binarytoint $ [epsilon, gamma] 
+solve x = product $ map binarytoint [epsilon, gamma] 
   where
     epsilon = map (fromEnum . (== 0)) gamma   
     gamma = map foo $ transpose x 
-    foo x = fromEnum $ sum x > ((length x) `div` 2)
+    foo x = fromEnum $ sum x > (length x `div` 2)
 
 main :: IO () 
 main = interact $ show . solve . map (map digitToInt) . lines  
--- 2021/day-03/part-02.hs
+++ 2021/day-03/part-02.hs
@@ -3,11 +3,11 @@ import Data.Char (digitToInt)
 
 binarytoint :: [Int] -> Int 
 binarytoint a = sum $ map (\(x,y) -> y*2^x) $ zip (reverse [0..n]) a
-  where n = (length a) - 1
+  where n = length a - 1
 
 rating :: [[Int]] -> Int -> String -> [Int] 
 rating a i r
-  | length f == 1 = f !! 0
+  | length f == 1 = head f
   | otherwise = rating f (i + 1) r 
   where
     f = filter ((== fromEnum sb).(!! i)) a
@@ -17,7 +17,7 @@ rating a i r
     x = (!! i) $ transpose a
 
 solve :: [[Int]] -> Int
-solve x = product $ map binarytoint $ [rating x 0 "life", rating x 0 "oxygen"] 
+solve x = product $ map binarytoint [rating x 0 "life", rating x 0 "oxygen"] 
 
 main :: IO () 
 main = interact $ show . solve . map (map digitToInt) . lines  
