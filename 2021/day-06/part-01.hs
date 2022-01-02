6ae1f6ca34e38f6d1f8c934ce22af2e4c759a87e
woodrock <j.r.h.wood98@gmail.com>
2022-01-02 22:43:40 +1300

    Day 6

---
+++ 2021/day-06/part-01.hs
@@ -0,0 +1,18 @@
+import Data.List.Split
+
+step :: [Int] -> [Int]
+step [a,b,c,d,e,f,g,h,i] = [b,c,d,e,f,g,h+a,i,a]
+
+nSteps n  = (!! n) . iterate step 
+
+count :: Int -> [Int] -> Int
+count a = length . filter (== a)
+
+fish :: [Int] -> [Int]
+fish x = map (`count` x) [0..8]
+
+main :: IO () 
+main = interact $ show . sum . nSteps 80 . fish . parse 
+    where 
+        parse = \a -> read $ "[" ++ a ++ "]" :: [Int]  
+
