45e6a2e9542735cced8f55e3de088cf4fffc6e3f
woodRock <j.r.h.wood98@gmail.com>
2023-12-14 16:10:28 +1300

    Day 4 - part 2 (attempted)

--- 2023/day-04/part-02.hs
+++ 2023/day-04/part-02.hs
@@ -1,22 +1,19 @@
 import Data.List.Split (splitOn)
+import Data.List (tails, sort, group)
+import Control.Applicative
 
 -- Here is one line of input.
 -- Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 5
 -- We can split on "|", then split on ":", then split on " " to get the numbers.
 
-type Score = Int
+type CardNo = Int -- ubique identifier for a card.
+type Score = Int -- number of matches for given card.
+type Scorecards = Int -- total number of score cards.
 type Winning = [Int]
 type Numbers = [Int]
+type Multiplier = Int -- Multiplier for each card.
 type Card = (Winning, Numbers)
-
-solve :: Score -> Card -> Score
-solve score ([], numbers) = score
-solve 0 (w:ws, numbers) -- First match is worth one point.
-    | w `elem` numbers = solve 1 (ws, numbers)
-    | otherwise = solve 0 (ws, numbers)
-solve score (w:ws, numbers) -- Score doubles after first match.
-    | w `elem` numbers = solve (score * 2) (ws, numbers)
-    | otherwise = solve score (ws, numbers)
+type Matches = (CardNo, [Int])
 
 parse :: String -> Card 
 parse x = (winning, numbers)
@@ -24,7 +21,22 @@ parse x = (winning, numbers)
         [winning_str, numbers_str] = splitOn "|" x
         right_of = head $ tail $ splitOn ":" winning_str
         numbers = map read $ words numbers_str :: Numbers
-        winning = map read $ words right_of :: Winning
+        winning = map read $ words right_of :: Winning 
 
-main:: IO() 
-main = interact $ show . sum . map (solve 0) . map parse . lines 
+score :: CardNo -> Score -> Card -> Matches
+score cn m ([], numbers) = (cn, [cn + 1 .. cn + m])
+score cn m (w:ws, numbers)
+    | w `elem` numbers = score cn (m + 1) (ws, numbers)
+    | otherwise = score cn m (ws, numbers)
+
+solve:: [Multiplier] -> [Matches] -> Scorecards
+solve multipliers matches = sum $ zipWith (*) multipliers $ map (length . snd) matches
+
+magic:: String -> String
+magic x = show $ matches
+    where 
+        matches = map (\(cn, c) -> score cn 0 c) . zip [1.. ] . map parse . lines $ x
+        multipliers = group . sort . concat . map snd $ matches
+
+main :: IO () 
+main = interact magic
