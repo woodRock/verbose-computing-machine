ae259d40f8be4febe1ea72fbea211d559eb48dc7
woodrock <j.r.h.wood98@gmail.com>
2023-12-18 19:00:55 +1300

    Day 4 - part 1

---
+++ 2023/day-05/part-02.hs
@@ -0,0 +1,30 @@
+import Data.List.Split (splitOn)
+import Data.List (sort, groupBy)
+
+type Map = [[Int]]
+type Seed = Int
+
+parseOne:: [String] -> Map
+parseOne (_:x) = (map read . words) <$> x
+
+parse :: String -> ([Seed],[Map])
+parse x = (seeds, maps)
+    where 
+        tmp_seeds = map read . words . head . tail . splitOn "seeds:" . head . lines $ x :: [Int]
+        read_seeds (x:y:xs) = [x..x+y] ++ read_seeds xs
+        seeds = read_seeds tmp_seeds
+        tmp_maps = filter ((/=) [""]) . groupBy (\a b -> a /= "" && b /= "") . tail . splitOn "\n" $ x
+        maps = parseOne <$> tmp_maps
+
+solveOne:: Seed -> [Map] -> Int
+solveOne seed [] = seed
+solveOne seed ([]:xs) = solveOne seed xs
+solveOne seed (([m,s,r]:maps):xs)
+    | seed >= s && seed < s + r = solveOne ((seed - s)+m) xs
+    | otherwise = solveOne seed ((maps):xs)
+
+solve:: ([Seed],[Map]) -> Int
+solve (seeds, maps) = minimum $ (\seed -> seed `solveOne` maps) <$> seeds
+        
+main:: IO() 
+main = interact $ show . solve . parse
