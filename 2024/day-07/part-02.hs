6da31b67b0bdc99fa244da9356050417d0ab2919
woodrock <j.r.h.wood98@gmail.com>
2024-12-07 19:34:46 +1300

    Refactor

--- 2024/day-07/part-02.hs
+++ 2024/day-07/part-02.hs
@@ -4,18 +4,11 @@ parse :: String -> (Int, [Int])
 parse s = (read v, map read $ words r)
     where (v, _:r) = span (/= ':') s
 
-ops :: [Int -> Int -> Int]
-ops = [(+), (*), \x y -> read $ show x ++ show y]
-
 solve :: (Int, [Int]) -> [Int]
-solve (v, []) = []
-solve (v, [x]) = [v | v == x]
 solve (v, ts) = [v | v `elem` results]
     where
-        opCombinations = sequence $ replicate (length ts - 1) [0..2]
-        evaluate ops' = foldl' (\acc (op, n) -> (ops !! op) acc n) 
-                              (head ts) 
-                              (zip ops' (tail ts))
+        opCombinations = sequence $ replicate (length ts - 1) [(+), (*), \x y -> read $ show x ++ show y]
+        evaluate ops = foldl (\a (f,n) -> f a n) (head ts) (zip ops (tail ts))
         results = map evaluate opCombinations
 
 main :: IO ()
