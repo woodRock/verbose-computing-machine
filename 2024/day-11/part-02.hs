f625e6bfbc813a83513d6459d62613aa1860013a
woodrock <j.r.h.wood98@gmail.com>
2024-12-11 19:05:22 +1300

    Refactor

--- 2024/day-11/part-02.hs
+++ 2024/day-11/part-02.hs
@@ -1,37 +1,17 @@
 import qualified Data.IntMap.Strict as IM
-import Data.List (foldl')
 
--- Rules carefully matching the specification
 rules :: Int -> [Int]
-rules 0 = [1]  -- Rule 1: If the stone is engraved with the number 0
-rules n = 
-    let s = show n  -- Int already ignores leading zeros
-        digitCount = length s
-    in if even digitCount
-       then  -- Rule 2: Even number of digits
-           let mid = digitCount `div` 2
-               (l, r) = splitAt mid s
-               leftNum = read l :: Int
-               rightNum = read r :: Int
-           in [leftNum, rightNum]
-       else [n * 2024]  -- Rule 3: Multiply by 2024
+rules 0 = [1]
+rules n 
+    | even len = [read l, read r]
+    | otherwise = [n * 2024]
+    where s = show n
+          len = length s
+          (l, r) = splitAt (len `div` 2) s
 
--- Process one generation using IntMap for efficiency
-blinkOnce :: IM.IntMap Int -> IM.IntMap Int
-blinkOnce freqMap = 
-    IM.foldlWithKey' processStone IM.empty freqMap
-    where
-        processStone !acc !stone !count =
-            let newStones = rules stone
-                addStone m s = IM.insertWith (+) s count m
-            in foldl' addStone acc newStones
-
--- Main processing function using IntMap
-process :: Int -> [Int] -> Int
-process generations initialStones = 
-    let initialMap = foldl' (\m n -> IM.insertWith (+) n 1 m) IM.empty initialStones
-        finalMap = (!! generations) $ iterate blinkOnce initialMap
-    in sum $ IM.elems finalMap
+blink :: IM.IntMap Int -> IM.IntMap Int
+blink = IM.foldlWithKey' (\m k v -> foldr (flip (IM.insertWith (+)) v) m (rules k)) IM.empty
 
 main :: IO ()
-main = interact $ show . process 75 . map read . words
+main = interact $ show . sum . IM.elems . (!! 75) . iterate blink . 
+       foldr (\k -> IM.insertWith (+) k 1) IM.empty . map read . words
