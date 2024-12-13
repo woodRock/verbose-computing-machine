0bea00ac4e61184a9c6404870a074a192b7f3fbe
woodrock <j.r.h.wood98@gmail.com>
2024-12-13 20:59:19 +1300

    Refactor: Matrix multiplication

--- 2024/day-13/part-02.hs
+++ 2024/day-13/part-02.hs
@@ -1,6 +1,23 @@
 import Text.Read (readMaybe)
 
+type Matrix = [[Int]]
+type Vector = [Int]
 type Machine = ((Int,Int), (Int,Int), (Int,Int))  -- ((ax,ay), (bx,by), (px,py))
+
+-- 2x2 matrix determinant
+determinant :: Matrix -> Int
+determinant [[a,b],[c,d]] = a*d - b*c
+
+-- 2x2 matrix inverse multiplied by determinant to keep integers
+-- Returns (matrix * det, det)
+matrixInverse :: Matrix -> (Matrix, Int)
+matrixInverse [[a,b],[c,d]] = 
+    let det = determinant [[a,b],[c,d]]
+    in ([[d,-b],[-c,a]], det)
+
+-- Matrix-vector multiplication
+matrixVectorMul :: Matrix -> Vector -> Vector
+matrixVectorMul m v = map (sum . zipWith (*) v) m
 
 parseMachine :: [String] -> Maybe Machine
 parseMachine [a,b,p] = do
@@ -17,24 +34,26 @@ parseInput = map (maybe (error "bad input") id . parseMachine) .
           f x (y:ys) = (x:y):ys
           f x [] = [[x]]
 
--- Solve linear equations:
--- ax * a + bx * b = px
--- ay * a + by * b = py
--- Returns token cost if solution exists with non-negative integers
-solveLinear :: Machine -> Maybe Int
-solveLinear ((ax,ay), (bx,by), (px,py)) = 
-    let det = ax*by - ay*bx  -- determinant
-    in if det == 0 
-       then Nothing  -- no unique solution
-       else let a = (px*by - py*bx) `div` det
-                b = (ax*py - ay*px) `div` det
-            in if a >= 0 && b >= 0 && 
-                  a*ax + b*bx == px && a*ay + b*by == py
-               then Just (3*a + b)
-               else Nothing
+-- Solve using matrix multiplication:
+-- [ax bx] [a] = [px]
+-- [ay by] [b]   [py]
+solveMatrix :: Machine -> Maybe Int
+solveMatrix ((ax,ay), (bx,by), (px,py)) = 
+    let matrix = [[ax,bx],[ay,by]]
+        vector = [px,py]
+        (inv, det) = matrixInverse matrix
+        -- Solution = inverse * vector / det
+        [a,b] = map (`div` det) $ matrixVectorMul inv vector
+    in if det == 0 || 
+          any ((/= 0) . (`mod` det)) (matrixVectorMul inv vector) ||
+          a < 0 || b < 0 
+       then Nothing
+       else if a*ax + b*bx == px && a*ay + b*by == py
+            then Just (3*a + b)
+            else Nothing
 
 main :: IO ()
 main = do
     machines <- parseInput <$> getContents
-    let tokens = sum . map (maybe 0 id) $ map solveLinear machines
+    let tokens = sum . map (maybe 0 id) $ map solveMatrix machines
     print tokens
