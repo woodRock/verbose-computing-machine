82cd1a9d2c9536affb276e6c912dab5c371bb68f
Jesse Wood <woodRock@users.noreply.github.com>
2024-12-20 09:59:57 +1300

    Update part-01.hs

--- 2024/day-19/part-01.hs
+++ 2024/day-19/part-01.hs
@@ -1,19 +1,32 @@
 import Data.List (isPrefixOf)
 import qualified Data.Map.Strict as Map
 import qualified Data.ByteString.Char8 as B
-import Control.DeepSeq (force)
+import qualified Data.Set as Set
+import Data.Maybe (isJust)
+import Control.Monad (guard)  -- Added this import
 
 type Pattern = B.ByteString
 type Design = B.ByteString
-type Cache = Map.Map B.ByteString Bool
-type PatternMap = Map.Map Char [Pattern]  -- Changed back to Map for safety
+type PatternMap = Map.Map Char [Pattern]
+
+data PQEntry = PQEntry {
+    position :: Int,
+    distance :: Int
+} deriving (Eq, Show)
+
+instance Ord PQEntry where
+    compare (PQEntry p1 d1) (PQEntry p2 d2) = 
+        compare d1 d2 <> compare p1 p2
 
 parse :: String -> ([Pattern], [Design])
 parse str = (patterns, designs)
   where
-    (towels_str, _:designs_str) = span (not . null) (lines str)
-    patterns = map B.pack $ splitOn ',' $ filter (/= ' ') $ head towels_str
-    designs = map B.pack $ filter (not . null) designs_str
+    ls = lines str
+    (towels_str, rest) = case ls of
+        [] -> ("", [])
+        (x:xs) -> (x, xs)
+    patterns = map B.pack $ splitOn ',' $ filter (/= ' ') towels_str
+    designs = map B.pack $ filter (not . null) rest
 
 splitOn :: Char -> String -> [String]
 splitOn c s = case dropWhile (== c) s of
@@ -27,24 +40,36 @@ buildPatternMap patterns = Map.fromListWith (++)
                 not (B.null p), 
                 let c = B.head p]
 
-canMakeDesign :: PatternMap -> Pattern -> Bool
-canMakeDesign patternMap design = go design Map.empty
+canMakeDesign :: PatternMap -> Design -> Bool
+canMakeDesign patternMap design = isJust $ dijkstra patternMap design
+
+dijkstra :: PatternMap -> Design -> Maybe Int
+dijkstra patternMap design = go initQueue initVisited
   where
-    go curr cache
-        | B.null curr = True
-        | otherwise = case Map.lookup curr cache of
-            Just result -> result
-            Nothing -> 
-                let c = B.head curr
-                    possible = Map.findWithDefault [] c patternMap
-                    validPatterns = filter (`B.isPrefixOf` curr) possible
-                    result = any continueWithPattern validPatterns
-                in result `seq` Map.insert curr result cache `seq` result
+    designLen = B.length design
+    initQueue = Set.singleton $ PQEntry 0 0
+    initVisited = Set.empty
+
+    go queue visited
+        | Set.null queue = Nothing
+        | position curr >= designLen = Just $ distance curr
+        | Set.member (position curr) visited = go queue' visited
+        | otherwise = go queue'' visited'
       where
-        continueWithPattern p = 
-            let remaining = B.drop (B.length p) curr
-            in B.length remaining < B.length curr && 
-               go remaining (Map.insert curr False cache)
+        curr = Set.findMin queue
+        queue' = Set.deleteMin queue
+        visited' = Set.insert (position curr) visited
+        
+        neighbors = do
+            let c = B.index design (position curr)
+            pattern <- Map.findWithDefault [] c patternMap
+            let newPos = position curr + B.length pattern
+            guard $ newPos <= designLen
+            guard $ pattern `B.isPrefixOf` B.drop (position curr) design
+            return $ PQEntry newPos (distance curr + 1)
+            
+        queue'' = foldr Set.insert queue' 
+            [n | n <- neighbors, not $ Set.member (position n) visited]
 
 solve :: String -> Int
 solve input = 
